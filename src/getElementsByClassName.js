c83a714053c61cc0f2131ad3257acff786139b37
jordanstholmes <jordanstholmes@gmail.com>
2018-09-17 16:02:04 -0700

    Solved getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,32 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+/*
+BASE:
+if it has no children
+  return whether or not it has the class name
+if it has children
+  return whether it has the class AND recurse on its children        
+*/
+var getElementsByClassName = function(className) {
+  
+  let checkElementChildren = function(element) {
+    if (element.nodeType === Node.TEXT_NODE) {
+      return [];
+    }
+        
+    let elemHasClass = element.classList.contains(className);
+    let result = elemHasClass ? [element] : [];
+    
+    if (!element.hasChildNodes()) {
+      return result;
+    } else {
+      return _.reduce(element.childNodes, function(acc, child) {
+        return acc.concat(checkElementChildren(child));
+      }, result);
+    }
+  };
+  
+  return checkElementChildren(document.body);  
 };
